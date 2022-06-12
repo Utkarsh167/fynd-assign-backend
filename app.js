c1613234b7a80c2ffca7f746e73b80da2ca95d39
Utkarshar16 <utkarshp@arkenea.com>
2022-06-13 02:42:57 +0530

    build 13/06/2022

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ var express = require('express')
 const CommonService = require('./common/commonService')
 
 // for http uncomment this code and comment above server
-// const httpserver = http.createServer(app).listen(httpport, () => {
-//   console.log('http server running at ' + httpport)
-// })
+const httpserver = http.createServer(app).listen(httpport, () => {
+  console.log('http server running at ' + httpport)
+})
 
-http.createServer(function (req, res) {
-  res.writeHead(301, {
-    "Location": "https://" + req.headers['host'] + req.url
-  });
-  res.end();
-}).listen(80)
+// http.createServer(function (req, res) {
+//   res.writeHead(301, {
+//     "Location": "https://" + req.headers['host'] + req.url
+//   });
+//   res.end();
+// }).listen(80)
 
 // Check if any user exist, if not then add admin
 var commonService = new CommonService()
