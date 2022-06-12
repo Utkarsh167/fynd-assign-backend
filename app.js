dfc2cf1ea58d3b09f8635abc029ba128e5be2744
Utkarshar16 <utkarshp@arkenea.com>
2022-06-13 03:08:47 +0530

    build 13/06/2022

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('./index')
 var debug = require('debug')('Baker:server')
 const http = require('http')
 // var port = normalizePort(process.env.PORT || '443') 
-var port = process.env.HTTPPORT || '80'
+var port = process.env.HTTPPORT || 3000
 
 var express = require('express')
 const CommonService = require('./common/commonService')
@@ -12,9 +12,10 @@ const CommonService = require('./common/commonService')
 //   console.log('http server running at ' + port)
 // })
 
-const httpserver = app.listen(port, () => {
-  console.log('http server running at ' + port)
-})
+app.listen(port, err => {
+  if(err) throw err;
+  console.log("%c Server running", "color: green");
+});
 // Check if any user exist, if not then add admin
 var commonService = new CommonService()
 commonService.checkAndAddAdmin();
