acd61ebc0f7e6f70f17c1cb3078f6234e9278f56
Utkarshar16 <utkarshp@arkenea.com>
2022-06-13 02:57:36 +0530

    build 13/06/2022

--- app.js
+++ app.js
@@ -2,32 +2,19 @@ var app = require('./index')
 var debug = require('debug')('Baker:server')
 const http = require('http')
 // var port = normalizePort(process.env.PORT || '443') 
-var httpport = process.env.HTTPPORT || '80'
-const https = require('https')
+var port = process.env.HTTPPORT || '80'
 
 var express = require('express')
 const CommonService = require('./common/commonService')
 
 // for http uncomment this code and comment above server
 // const httpserver = http.createServer(app).listen(httpport, () => {
-//   console.log('http server running at ' + httpport)
+//   console.log('http server running at ' + port)
 // })
 
-var httpsOptions = {
- 
-}   
-var server = https.createServer(httpsOptions, app).listen(443, function () {
-  console.log("Express server listening on port " + 443);
-});
-
-
-// http.createServer(function (req, res) {
-//   res.writeHead(301, {
-//     "Location": "https://" + req.headers['host'] + req.url
-//   });
-//   res.end();
-// }).listen(80)
-
+const httpserver = app.listen(httpport, () => {
+  console.log('http server running at ' + port)
+})
 // Check if any user exist, if not then add admin
 var commonService = new CommonService()
 commonService.checkAndAddAdmin();
